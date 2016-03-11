792f2330d28d546124814b81156af704b2d85093
dmeeking <dan.meeking@shopify.com>
2016-03-11 00:30:08 -0500

    tweaking range

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,8 +7,9 @@ class HomeController < ApplicationController
 
   def temperature_readings
 
-  	chartRange = 1.days.ago.midnight..Date.tomorrow.tomorrow.midnight
-  	forecastRange = Time.now..Date.tomorrow.tomorrow.midnight
+ # 	chartRange = 1.days.ago.midnight..Date.tomorrow.tomorrow.midnight
+  	chartRange = 1.days.ago.midnight..-23.hours.ago
+  	forecastRange = Time.now..-23.hours.ago
   	dateFormat = '%a, %d %b %Y %H:%M:%S %z';
 
   	hourlyForecast = HourlyForecast.group_by_hour(:reading_at, format:dateFormat, range: forecastRange, default_value:nil).average(:temperature)
@@ -17,15 +18,34 @@ class HomeController < ApplicationController
 # Todo: Someday we can figure out how to return multiple aggregates in group_by
   	yowReadings = WeatherReading.group_by_hour(:reading_at, format:dateFormat, range: chartRange, default_value:nil).average(:temperature)
 	windSpeed = WeatherReading.group_by_hour(:reading_at, format:dateFormat, range: chartRange, default_value:nil).average(:wind_speed)
+	#windDirection = WeatherReading.group_by_hour(:reading_at, format:dateFormat, range: chartRange, default_value:nil).minimum(:wind_direction)
+
+	#ar2 = windDirection.map {|a|
+	#	[a[0], degrees_from_direction(a[1])]
+		
+	#}
+	
+
 
   	highchartsData = {
   		yowReadings: yowReadings,
   		hourlyForecast: hourlyForecast,
   		windSpeed: windSpeed,
-  		windSpeedForecast: windForecast
+  		windSpeedForecast: windForecast,
+  		#windDirection: ar2
   	}
 
   	render json: highchartsData
   end
+
+  def degrees_from_direction(direction)
+  	 directions = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+  	 index = directions.index(direction)
+  	 index = index.nil? ? 0 : index
+  	 
+  	 11.5 + (index * 22.5)
+
+  end
+
 
 end
