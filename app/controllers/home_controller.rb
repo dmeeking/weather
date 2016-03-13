88cc950a03de76cf9e7c3a185469bc8db677df84
dmeeking <dan.meeking@shopify.com>
2016-03-13 11:21:43 -0400

    much cleaner data querying and json array passing from controller to view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,42 +7,21 @@ class HomeController < ApplicationController
 
   def temperature_readings
 
-  	chartRange = 1.days.ago.midnight..-23.hours.ago
+  	chartRange = 1.days.ago.midnight..-24.hours.ago
     chartRangePressure = 1.days.ago.midnight..Time.now
   	dateFormat = '%a, %d %b %Y %H:%M:%S %z';
 
-   # Works! Will require some (easy) refactoring of client-side code to make it work fully
-   # allReadings = WeatherReading.where(reading_at: chartRange).order(reading_at: :asc).select(:reading_at, :temperature, :pressure, :wind_speed, :wind_direction).to_a
-   # allReadings = allReadings.group_by_hour(format:dateFormat, default_value:nil) {|r| r.reading_at}
+    allReadings = WeatherReading.where(reading_at: chartRange).order(reading_at: :asc).select(:reading_at, :temperature, :pressure, :wind_speed, :wind_direction).to_a
+    allReadings = allReadings.group_by_hour(format:dateFormat, default_value:nil) {|r| r.reading_at}
 
-  	yowReadings = WeatherReading.group_by_hour(:reading_at, format:dateFormat, range: chartRange, default_value:nil).average(:temperature)
-	  windSpeed = WeatherReading.group_by_hour(:reading_at, format:dateFormat, range: chartRange, default_value:nil).average(:wind_speed)
-    pressureReadings = WeatherReading.group_by_hour(:reading_at, format:dateFormat, range: chartRangePressure, default_value:nil).average(:pressure)
-	  windDirection = WeatherReading.group_by_hour(:reading_at, format:dateFormat, range: chartRange, default_value:nil).minimum(:wind_direction)
+    forecastRange = WeatherReading.order(reading_at: :desc).limit(1).first.reading_at..-24.hours.ago
+    allForecasts = HourlyForecast.where(reading_at: forecastRange).order(reading_at: :asc).select(:reading_at, :temperature, :wind_speed).to_a
+    allForecasts = allForecasts.group_by_hour(format:dateFormat, default_value:nil) {|r| r.reading_at}
 
-    forecastRange = WeatherReading.order(reading_at: :desc).limit(1).first.reading_at..-23.hours.ago
-  
-    hourlyForecast = HourlyForecast.group_by_hour(:reading_at, format:dateFormat, range: forecastRange, default_value:nil).average(:temperature)
-    windForecast = HourlyForecast.group_by_hour(:reading_at, format:dateFormat, range: forecastRange, default_value:nil).average(:wind_speed)
-
-
-
-
-  	highchartsData = {
-  		yowReadings: yowReadings,
-  		hourlyForecast: hourlyForecast,
-  		windSpeed: windSpeed,
-  		windSpeedForecast: windForecast,
-      pressureReadings: pressureReadings
-  		#windDirection: ar2
-  	}
-
-  	render json: highchartsData
-
-    #render json: {
-     #   observed: allReadings,
-     #   forecast: TODO
-     # }
+    render json: {
+        observed: allReadings,
+        forecast: allForecasts
+      }
   end
 
   def degrees_from_direction(direction)
