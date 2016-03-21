b067e4ff27d20eb0e480a2810940715cf47d5fe8
dmeeking <dan.meeking@shopify.com>
2016-03-21 08:30:41 -0400

    debugging

--- app/assets/javascripts/focus-watch.js
+++ app/assets/javascripts/focus-watch.js
@@ -1,10 +1,12 @@
 var blurred = false;
 var timeOfBlur = 0;
 window.onblur = function() {
+    console.log('blur');
     blurred = true;
     timeOfBlur = new Date().getTime();
 };
 window.onfocus = function() {
+    console.log('focus',blurred,timeOfBlur );
     if(blurred && timeOfBlur > 0){
         var now = new Date().getTime();
         var refreshTime = 5*60*1000; //5 minutes
